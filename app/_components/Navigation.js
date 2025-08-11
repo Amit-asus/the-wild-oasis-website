0acece5cea74ef19ce74a908edebc47011fec1a7
amit-mac <144879811+Amit-asus@users.noreply.github.com>
2025-08-11 13:01:17 +0530

    added the starter code

---
+++ app/_components/Navigation.js
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+export default function Navigation() {
+  return (
+    <nav className="z-10 text-xl">
+      <ul className="flex gap-16 items-center">
+        <li>
+          <Link
+            href="/cabins"
+            className="text-white hover:text-accent-400 transition-colors "
+          >
+            Cabins
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className="text-white hover:text-accent-400 transition-colors"
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/account"
+            className="text-white hover:text-accent-400 transition-colors"
+          >
+            Guest area
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const metadata =  {
+  title: 'cabins' 
+}
+
+
+const page = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default page;
